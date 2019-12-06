9005c48c8e9840fa9e2389334d32666a128eae16
PurplePokey <46290939+PurplePokey@users.noreply.github.com>
2019-12-05 19:02:22 -0600

    Fixed CSV Reader to store all data in List. Added menu options
    Menu option a requires additional error handling, indicated by comment. Menu option r is complete, unless files should not be overwritten. Menu option t is incomplete. CSVReader2 has been merged onto CSVReader and is now unecessary

--- CSVReader.java
+++ CSVReader.java
@@ -1,46 +1,64 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import java.util.List;
+/*
+ *File name: CSVReader2.java
+ * 
+ *Programmer: Raph Rusak
+ *Date: Dec 5, 2019
+ */
 
 /**
  * <insert class description here>
- * 
- * @author 
+ *@author Raph Rusak
  *
  */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVReader {
+	// will read the information on the csv file and separate it all into individual
+	// lines
+	public List<String> readData(String csvFile) {
 
-	/**
-	 * @param args
-	 */
-	public String[] readData(String filename) throws IOException {
-
-		// reads the date and message from a file and puts it into an ArrayList
-		FileReader fileReader = new FileReader(filename);
-
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+		String[] temp = null;
 		List<String> data = new ArrayList<String>();
-		String line = null;
 
-		while ((line = bufferedReader.readLine()) != null) {
-			data.add(line);
-		}
 
-		bufferedReader.close();
+		try {
 
-		String[] bioData = new String[data.size()];
-		// takes each string from the array and divides it into specific
-		// components
-		if (!data.get(0).equals("")) {
-			for (int i = 0; i < data.size(); i++) {
-				
+			br = new BufferedReader(new FileReader(csvFile));
+			// two while loops to skip the first line of the category titles
+			while ((line = br.readLine()) != null) {
+				while ((line = br.readLine()) != null) {
+					// use comma as separator
+					temp= line.split(cvsSplitBy);
+					//printing for test
+					for (int i = 0; i < temp.length; i++) {
+						data.add(temp[i]);
+					}
 
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
-		return bioData;
-	}
+		return data;
 
+	}
 }
--- Driver.java
+++ Driver.java
@@ -1,3 +1,8 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Driver {
@@ -5,34 +10,182 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		String userInput="";
-		String fileName = "";
+		String fileNameRead = "";
+		String fileNameWrite = "";
 		String rela = "";
+		String strTemp = "";
+		String cuiTemp = "";
+		String medicalTerm = "";
+		List<String> dataTemp=null;
+		List<String> dataComplete=new ArrayList<String>();
+		List<String> terms = new ArrayList<String>();
+		
+		FileWriter csvWrite=null;
 		
 		Scanner scan = new Scanner(System.in);
 		CSVReader reader = new CSVReader();
 		
+		//read data into terms
+		terms=reader.readData("terms.csv");
 		
 		while(!userInput.equalsIgnoreCase("e")) {
 			displayMenu();
-			userInput=scan.next();
-			
+			userInput=scan.nextLine();
+
+			//MENU OPTION A - ADDING DATA
 			if (userInput.equalsIgnoreCase("a")) {
 				
+				//get filename for reading part i - COMPLETE
+				System.out.println("Enter the file name: ");
+				fileNameRead=scan.nextLine();
+				
+				//read file into dataComplete part ii
+				//ERROR HANDLING FOR INVALID FILENAME NEEDED
+				dataTemp = reader.readData(fileNameRead);
+				for(int i = 0; i<dataTemp.size();i++) {
+						dataComplete.add(dataTemp.get(i));
+					
+				}
+				
+				//Print number of rows read part iii
+				//NEED TO CHECK IF FILE HAS BEEN PROCESSED YET
+				System.out.println(dataTemp.size() + " rows read.");
+				
 			}
+			
+			//MENU OPTION R - SAVE DATA FOR A RELATIONSHIP 
 			else if (userInput.equalsIgnoreCase("r")) {
 				
-			}
-			else if (userInput.equalsIgnoreCase("t")) {
+				//get relationship type part i - COMPLETE
+				System.out.println("Enter the relationship type: ");
+				rela=scan.nextLine();
+				
+				//get file name for export part ii - COMPLETE
+				System.out.println("Enter the file name: ");
+				fileNameWrite=scan.nextLine();
+				
+				//find all data for the relationship type and write output file part iii
+				//create output file
+				try {
+					csvWrite = new FileWriter(fileNameWrite);
+					//Create columns
+					//appends STR 
+					csvWrite.append("STR");
+					csvWrite.append(",");
+					//appends RELATIONSHIP
+					csvWrite.append("Relationship");
+					csvWrite.append(",");
+					//appends STR2
+					csvWrite.append("STR2");
+					csvWrite.append("\n");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				//find relationship types and write to output file - COMPLETE - CHECK BUG
+				// BUG??? -> overwrites file every time option is selected instead of appending
+				for(int i =0; i<dataComplete.size();i++) {
+					if(rela.equals(dataComplete.get(i))) {
+						
+						try {
+							//find STR for CUI
+							cuiTemp=dataComplete.get(i-1);
+							for(int t = 0; t<terms.size(); t++) {
+								if(cuiTemp.equals(terms.get(t))) {
+									strTemp=terms.get(t+1);
+								}
+							}
+							//appends STR 
+							csvWrite.append(strTemp);
+							csvWrite.append(",");
+							//appends RELATIONSHIP
+							csvWrite.append(dataComplete.get(i));
+							csvWrite.append(",");
+							//appends STR2
+							csvWrite.append(dataComplete.get(i+2));
+							csvWrite.append("\n");
+						} catch (IOException e) {
+							e.printStackTrace();
+						}	
+					}
+				}
 				
 			}
+			
+			//MENU OPTION T - SAVE DATA FOR MEDICAL TERMS
+			else if (userInput.equalsIgnoreCase("t")) {
+				
+				//get medical term part i - COMPLETE
+				System.out.println("Enter the medical term: ");
+				medicalTerm=scan.nextLine();
+				
+				//get file name for export part ii - COMPLETE
+				System.out.println("Enter the file name: ");
+				fileNameWrite=scan.nextLine();
+				
+				//find all data for the relationship type and write output file part iii
+				//create output file
+				try {
+					csvWrite = new FileWriter(fileNameWrite);
+					//Create columns
+					//appends STR 
+					csvWrite.append("STR");
+					csvWrite.append(",");
+					//appends RELATIONSHIP
+					csvWrite.append("Relationship");
+					csvWrite.append(",");
+					//appends STR2
+					csvWrite.append("STR2");
+					csvWrite.append("\n");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				//find medical term and write to output file
+				// BUG??? -> overwrites file every time option is selected instead of appending
+				for(int i =0; i<dataComplete.size();i++) {
+					if(medicalTerm.equals(dataComplete.get(i))) {
+						try {
+							//appends STR 
+							csvWrite.append(dataComplete.get(i-1));
+							csvWrite.append(",");
+							//appends RELATIONSHIP
+							csvWrite.append(dataComplete.get(i));
+							csvWrite.append(",");
+							//appends STR2
+							csvWrite.append(dataComplete.get(i+1));
+							csvWrite.append("\n");
+						} catch (IOException e) {
+							e.printStackTrace();
+						}	
+					}
+				}
+			}
+			
+			
+			//EXIT PROGRAM - COMPLETE
 			else if(userInput.equalsIgnoreCase("e")) {
+
 				System.out.println("Quitting application.");
+				
 			}
+			
+			
+			//INVALID MENU SELECTION
 			else {
+				
 				System.out.println("Invalid choice, try again.\n");
+				
 			}
 			
+
 			
+		}
+		try {
+			csvWrite.flush();
+			csvWrite.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
 	}
